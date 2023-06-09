e0ee07975b8771825d4b5b5a82be66372b8622b8
Aline Silva <106278948+AlineSilvaFerreira@users.noreply.github.com>
2023-06-08 22:30:55 -0300

    botao colar e tirar acentos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const texto = document.querySelector(".texto");
 const mensagem = document.querySelector(".msg-encriptada");
+var colar = document.querySelector(".btn-colar");
 var copiar = document.querySelector(".btn-copiar");
 var itens = document.querySelector(".itens")
 
@@ -10,53 +11,72 @@ var itens = document.querySelector(".itens")
 // `A letra "o" é convertida para "ober"`
 // `A letra "u" é convertida para "ufat"`
 
-
-
 function btnEncriptar() {
-    const textoEncriptado = encriptar(texto.value);
-    mensagem.value = textoEncriptado;
-    texto.value = "";
+    if(!texto.value || texto.value == " "){
+        alert("Nenhuma mensagem encontrada");
 
-    mensagem.style.display = "block";
-    copiar.style.display = 'block';
-    itens.style.display = 'none'; 
-    
+    }else{
+        const textoEncriptado = encriptar(texto.value);
+        mensagem.value = textoEncriptado;
+        texto.value = "";
+        
+        mensagem.style.display = "block";
+        copiar.style.display = 'block';
+        itens.style.display = 'none'; 
+    }
+
+    document.getElementById("btn-copiar").innerHTML="Copiar";
 }
 
-
 function encriptar(stringEncriptada) {
+    var textoDigitado = stringEncriptada.replace(/[áàâãä]/g, "a")
+    .replace(/[éèêë]/g, "e")
+    .replace(/[íìîï]/g, "i")
+    .replace(/[óòôõö]/g, "o")
+    .replace(/[úùûü]/g, "u")
+    .replace(/[ç]/g, "c");  
 
+    var stringEncriptada = textoDigitado.toLowerCase();
     let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase();
-
+    
     for(let i = 0; i < matrizCodigo.length; i++) {
         if(stringEncriptada.includes(matrizCodigo[i][0])) {
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
     }
-
     return stringEncriptada;
-
-
 }
 
-
 function btnDesencriptar() {
-    const textoDesencriptado = desencriptar(texto.value);
-    mensagem.value = textoDesencriptado;
-    texto.value = "";
+    if(!texto.value || texto.value == " "){
+        alert("Nenhuma mensagem encontrada");
 
-    mensagem.style.display = "block";
-    copiar.style.display = 'block';
-    itens.style.display = 'none'; 
+    }else{
+        const textoDesencriptado = desencriptar(texto.value);
+        mensagem.value = textoDesencriptado;
+        texto.value = "";
+
+        mensagem.style.display = "block";
+        copiar.style.display = 'block';
+        itens.style.display = 'none'; 
+    }
+
+    document.getElementById("btn-copiar").innerHTML="Copiar";
 }
 
 
 function desencriptar(stringDesencriptada) {
+    var text = stringDesencriptada.replace(/[áàâãä]/g, "a")
+    .replace(/[éèêë]/g, "e")
+    .replace(/[íìîï]/g, "i")
+    .replace(/[óòôõö]/g, "o")
+    .replace(/[úùûü]/g, "u")
+    .replace(/[ç]/g, "c");  
+
+    var stringDesencriptada = text.toLowerCase();
 
     let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"]];
-    stringDesencriptada = stringDesencriptada.toLowerCase();
-
+   
     for(let i = 0; i < matrizCodigo.length; i++) {
         if(stringDesencriptada.includes(matrizCodigo[i][1])) {
             stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
@@ -64,16 +84,21 @@ function desencriptar(stringDesencriptada) {
     }
 
     return stringDesencriptada;
+
 }
 
-function btnCopiar() {
-	
-	var textToCopy = mensagem.value;
-  
-	navigator.clipboard.writeText(textToCopy);
+copiar.addEventListener('click', (e) => {
+    e.preventDefault();
 
-    alert("Texto copiado para a área de transferência");
+    navigator.clipboard.writeText(mensagem.value);
     
-  } 
+    document.getElementById("btn-copiar").innerHTML="Copiado!";
+
+});
+
+colar.addEventListener('click', async (e) => {
+    const colar = await navigator.clipboard.readText();//     
+    texto.value = colar;
+});
 
 
